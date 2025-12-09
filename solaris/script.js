695ce4ddcf0d2525a24542c16b9b0fae0e94ab8f
Nehac210401 <chatrathneha2@gmail.com>
2025-12-09 23:19:33 +0100

    Update script.js
    Including comments on the code.

--- solaris/script.js
+++ solaris/script.js
@@ -1,45 +1,45 @@
+//Hämtar alla planet-element i listan.
 const planets = document.querySelectorAll(".planet");
 const container = document.querySelector(".container");
+
+//Hämtar overlay och stäng i konen.
 const overlay = document.getElementById("overlay");
 const overlayContent = document.getElementById("overlayContent");
-const searchBtn = document.getElementById("searchBtn");
-const searchInput = document.getElementById("searchInput");
-const searchResult = document.getElementById("searchResult");
 const closeIcon = document.querySelector(".close-icon");
 
-searchBtn.addEventListener("click", function (event){
-    getPlanetData();
-})
-
+//Klick på stäng-ikonen döljer overlay.
 closeIcon.addEventListener("click", ()=>{ 
       overlay.classList.add("hidden");
-      console.log("hello");
 
 });
 
+//Hämtar API nyckel.
 async function fetchKey(){
     try{
         let response = await fetch('https://4a6l0o1px9.execute-api.eu-north-1.amazonaws.com/key');
         console.log(response);
-        let data = await response.json();
+        let data = await response.json();               //Väntar svar i JSON form.
         let key = data.key;
         console.log("key " + key);
         console.log(data);
-        return key;
+        return key;                                  //Returnerar API nycklen
     }
     catch (error) {
         console.log("Wrong fetchkeys");
     }
 }
 
+//Hämtar alla planeter från API och returnerar den som matchar klickad planet.
 async function getPlanetData(name){
     //const key = await getApiKey();
     const response = await fetch ("https://corsproxy.io/?https://4a6l0o1px9.execute-api.eu-north-1.amazonaws.com/bodies", {
         method: 'GET',
         headers: {'x-zocom' :'solaris-JAaNDtW2DyvIHS96' }
 });
+
+
     const data = await response.json();
-    const bodies = data.bodies;
+    const bodies = data.bodies;                    //Lista av alla planeter från API
     const planet = bodies.find;
     console.log(data);
     console.log("CLICKED NAME =", name);
@@ -49,35 +49,22 @@ async function getPlanetData(name){
     return bodies.find(p=> p.name.toLowerCase() === name.toLowerCase());
 }
 
-   //if (!data.bodies || data.bodies.length === 0){
-    //    searchResult.innerHTML = "Planet not found!";
-    //    return;
-    //}
-    //return data.bodies[0];
-
+// Hämtar planetet data från API
 getPlanetData();
 
+//Lägger klick event på varje planet i listan
 planets.forEach (planet => {
     planet.addEventListener("click", async function(){
-        const name = planet.dataset.name;
-        const data = await getPlanetData(name);
-        overlay.classList.remove("hidden");
-        displayPlanetInfo(data);
+        const name = planet.dataset.name;           //Hämtar data namnet från li element från HTML
+        const data = await getPlanetData(name);     //Hämtar motsvarnade planetdata från API.
+        overlay.classList.remove("hidden");         //Visar overlay med information.
+        displayPlanetInfo(data);                   //Fyller overlay med planetens information.
    });
 
 });
 
-//searchBtn.addEventListener("click", async function(){
-   //const name = searchInput.value.trim();
-    //if (name === ""){
-     //   searchResult.innerHTML = "Please enter the planet //name";
-      //  return;
-    //}
 
-   //const data = await getPlanetData(name);
-    //displayPlanetInfo(data);
-//});
-
+//Skapar en funtion för att overlay med data från API
    function displayPlanetInfo(data) {
     document.getElementById("planetName").innerHTML = data.name;
     document.getElementById("latinName").innerText = data.latinName;
@@ -91,6 +78,7 @@ planets.forEach (planet => {
     document.getElementById("planetOrbit").textContent = "Orbitalperiod: " + data.orbitalPeriod;
     document.getElementById("planetId").textContent = "ID: " + (data.id);
 
+    //Säkerställer att overlay verkligen visas.
     overlay.classList.remove("hidden");
 
    }
